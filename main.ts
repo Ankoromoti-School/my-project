291d5141c4e1651608f9741cd97ba3925740f42b
Ankoromoti-School <115124264+Ankoromoti-School@users.noreply.github.com>
2022-10-07 20:19:44 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.AB, function () {
+    カウント = 0
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    basic.showString("" + (カウント))
 })
+input.onGesture(Gesture.Shake, function () {
+    カウント += 1
+    music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
+    basic.showString("" + (カウント))
+})
+let カウント = 0
+カウント = 0
